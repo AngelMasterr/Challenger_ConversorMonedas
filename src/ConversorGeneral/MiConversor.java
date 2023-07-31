1b9dc243fbc77c09ddf93185bb35a41d05b15605
AngelMasterr <angelduarteprog@gmail.com>
2023-07-30 21:50:37 -0500

    Terminando la ubicacion de los objetos

--- src/ConversorGeneral/MiConversor.java
+++ src/ConversorGeneral/MiConversor.java
@@ -2,10 +2,18 @@ package ConversorGeneral;
 
 import java.awt.EventQueue;
 import javax.swing.JFrame;
+import javax.swing.JTextField;
+import javax.swing.JComboBox;
+import javax.swing.JButton;
+import javax.swing.JLabel;
+import javax.swing.JTextPane;
+import java.awt.Font;
+import javax.swing.SwingConstants;
 
 public class MiConversor {
 
-	private JFrame frame;
+	private JFrame frmConversor;
+	private JTextField textField;
 
 	/**
 	 * Launch the application.
@@ -15,7 +23,7 @@ public class MiConversor {
 			public void run() {
 				try {
 					MiConversor window = new MiConversor();
-					window.frame.setVisible(true);
+					window.frmConversor.setVisible(true);
 				} catch (Exception e) {
 					e.printStackTrace();
 				}
@@ -34,10 +42,38 @@ public class MiConversor {
 	 * Initialize the contents of the frame.
 	 */
 	private void initialize() {
-		frame = new JFrame();
-		frame.setBounds(100, 100, 450, 300);
-		frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+		frmConversor = new JFrame();
+		frmConversor.setTitle("Conversor");
+		frmConversor.setBounds(100, 100, 400, 250);
+		frmConversor.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+		frmConversor.getContentPane().setLayout(null);
+		
+		textField = new JTextField();
+		textField.setBounds(149, 113, 86, 20);
+		frmConversor.getContentPane().add(textField);
+		textField.setColumns(10);
+		
+		JComboBox comboBox = new JComboBox();
+		comboBox.setBounds(226, 66, 86, 22);
+		frmConversor.getContentPane().add(comboBox);
+		
+		JButton btnNewButton = new JButton("Convertir");
+		btnNewButton.setBounds(149, 156, 86, 20);
+		frmConversor.getContentPane().add(btnNewButton);
+		
+		JLabel titulo = new JLabel("Seleccione las divisas a convertir");
+		titulo.setFont(new Font("Tahoma", Font.PLAIN, 14));
+		titulo.setBounds(94, 20, 195, 16);
+		frmConversor.getContentPane().add(titulo);
+		
+		JLabel lblNewLabel_1 = new JLabel("<----->");
+		lblNewLabel_1.setHorizontalAlignment(SwingConstants.CENTER);
+		lblNewLabel_1.setBounds(169, 70, 46, 14);
+		frmConversor.getContentPane().add(lblNewLabel_1);
+		
+		JComboBox comboBox_1 = new JComboBox();
+		comboBox_1.setBounds(70, 66, 86, 22);
+		frmConversor.getContentPane().add(comboBox_1);
 	}
-
 }
 
